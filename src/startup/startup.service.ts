a30bc14a81ba878c7722779d0634809cc2bae4f2
sofiene chihi <sofienechihi@insat.u-carthage.tn>
2021-12-05 09:40:41 +0100

    get startups

--- src/startup/startup.service.ts
+++ src/startup/startup.service.ts
@@ -3,26 +3,17 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { CreateStartupDto } from './dto/createStartup.dto';
 import { Startup } from './startup.entity';
-import * as data from "../../data.json"
+import * as data from '../../data.json';
 
 @Injectable()
 export class StartupService {
+  constructor(
+    @InjectRepository(Startup) private startupRepository: Repository<Startup>,
+  ) {}
 
-    constructor(
-        @InjectRepository(Startup) private startupRepository: Repository<Startup>,
-      ) {}
-    
-      async findAll(): Promise<Startup[]> {
-        return await this.startupRepository.find();
-      }
-
-
-      async createStartup(): Promise<Startup[]> {
-
-        const newStartups : Startup[] = data.map((startup : CreateStartupDto)=>{
-            return this.startupRepository.create(startup)
-        })
-    
-        return await this.startupRepository.save(newStartups);
-      }
+  async findAll(): Promise<Startup[]> {
+    return await this.startupRepository.find({
+      take: 100,
+    });
+  }
 }
